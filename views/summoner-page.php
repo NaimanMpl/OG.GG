8e81c26baee5863aeb070b2118ec34c1e2080761
Axel <117041590+Drag0nQQ@users.noreply.github.com>
2023-10-05 21:52:00 +0200

    Added summoner-page (HTML and CSS)

---
+++ views/summoner-page.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="summoner-page.css">
+    <title>OG.GG</title>
+</head>
+<body>
+    <header>
+        <div class="nav-pages">
+            <a href="/" class="hessgg">OG.GG</a>
+            <nav>
+                <ul>
+                    <li><a href="/classment"></a>Classement</li>
+                    <li><a href="/chat"></a>Chat</li>
+                </ul>
+            </nav>
+        </div>
+        <div class="nav-pages">
+            <div class="player-research">
+                <svg class="search_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" alt="search icon">
+                    <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z">
+                    </path>
+                </svg>
+                <input type="text" placeholder="Rechercher un joueur">
+            </div>
+            <input type="button" onclick="location.href='/login'" value="Se connecter">
+        </div>
+    </header>
+    <div class="wrapper-banner">
+        <section>
+            <div id="pp">
+                <img src="image 10.svg" alt="Profil Picture">
+                <div class="level">
+                    <span class="level">426</span>
+                </div>
+            </div>
+            <div id="title">
+                <h1>Dragonkuku</h1>
+                <span>S13 Challenger</span>
+                <span>Être supérieur</span>
+                <span>Dieu vivant</span>
+            </div>
+            <input type="button" value="Mettre à jour">
+        </section>
+    </div>
+</body>
+</html>
